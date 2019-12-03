4438dc544db22e53c5bfb8dfe14b0e91b36bc060
Munky665 <adam.b.whitty@gmail.com>
2019-12-03 21:03:45 +1100

    Added Unity Project, also added basic layout to scene, added movement controls for pc use

---
+++ DuckHuntGame/DuckHunt/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float sensitivity = 5;
+    [SerializeField]
+    private float smoothing = 2;
+
+    Vector2 smoothV;
+    Vector2 mouseLook;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        player = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    #if UNITY_EDITOR
+        Aim();
+    #endif
+    }
+
+    void Aim()
+    {
+        //get axis and store in vector
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        //scale the vector
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+
+        mouseLook += smoothV;
+        //clamp the look up rotation at 90 and -90
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90, 90);
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+
+    }
+}
